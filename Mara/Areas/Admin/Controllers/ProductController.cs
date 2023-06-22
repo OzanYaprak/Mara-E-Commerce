72136d079b4031cf8aa832e57e9544c77cc63e09
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-06-23 01:09:10 +0300

    Update

--- Mara/Areas/Admin/Controllers/ProductController.cs
+++ Mara/Areas/Admin/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Mara.Areas.admin.Controllers
 {
+    
     [Area("admin"), Authorize]
     public class ProductController : Controller
     {
@@ -18,11 +19,63 @@ namespace Mara.Areas.admin.Controllers
 
 
 
-
         [Route("ÜrünYönetimi")]
         public IActionResult Index()
         {
+            var products = _productRepository.GetAll().OrderBy(a => a.ID);
+
+            return View(products);
+        }
+
+
+
+
+        [Route("YeniÜrünEkle")]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [Route("YeniÜrünEkle")]
+        public async Task<IActionResult> Create(Product model)
+        {
+            await _productRepository.AddAsync(model);
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        [HttpGet]
+        [Route("ÜrünGüncelle")]
+        public async Task<IActionResult> Update(int id)
+        {
+            var getProductToUpdate = await _productRepository.GetByAsync(a => a.ID == id);
+
+            return View(getProductToUpdate);
+        }
+
+        [HttpPost]
+        [Route("ÜrünGüncelle")]
+        public async Task<IActionResult> Update(Product model)
+        {
+            await _productRepository.UpdateAsync(model);
+
+            return RedirectToAction("Index");
+        }
+
+
+
+
+        [Route("ÜrünSil")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _productRepository.GetByAsync(x => x.ID == id);
+
+            await _productRepository.DeleteAsync(product);
+
+            return RedirectToAction("Index");
         }
     }
 }
